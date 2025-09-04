1a40d5205465e2ddf768c21fb5f5c52f80f85089
tilav-web <shavqiddintilovov0@gmail.com>
2025-09-04 22:40:18 +0500

    .

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors({
-    origin: 'http://localhost:3000',
+    origin: 'https://uygunlik.uz',
     credentials: true,
   });
   app.use(cookieParser());
