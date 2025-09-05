8bf68b535b8ae5e017fac42b37802849afddb3c0
tilav-web <shavqiddintilovov0@gmail.com>
2025-09-05 14:07:41 +0500

    .

--- src/main.ts
+++ src/main.ts
@@ -2,15 +2,21 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
 import { ValidationPipe } from '@nestjs/common';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors({
     origin: ['https://uygunlik.uz', 'https://www.uygunlik.uz'],
     credentials: true,
   });
   app.use(cookieParser());
+  app.useStaticAssets(join(__dirname, '..', 'uploads'), {
+    prefix: '/uploads/',
+  });
+
   await app.listen(process.env.PORT ?? 3001);
 }
 bootstrap();
