4ff28f66dd9a6f88fd59df500b8f11e234a697c5
tilav-web <shavqiddintilovov0@gmail.com>
2025-09-12 18:02:00 +0500

    .

--- src/main.ts
+++ src/main.ts
@@ -8,12 +8,6 @@ import * as bodyParser from 'body-parser';
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.useGlobalPipes(new ValidationPipe());
-
-  app.enableCors({
-    origin: ['https://uygunlik.uz'],
-    credentials: true,
-  });
-
   app.use(cookieParser());
   app.use(bodyParser.json({ limit: '2gb' }));
   app.use(bodyParser.urlencoded({ limit: '2gb', extended: true }));
